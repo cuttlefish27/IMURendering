6484a218f7638f7d4b2528d55b714cdd3817ac70
cuttlefish27 <Sahil.Vijay624@gmail.com>
2026-06-08 22:37:17 -0700

    importing tkinter to create a control interface

--- viz.py
+++ viz.py
@@ -1,20 +1,21 @@
 import serial
+from tkinter import *
+from tkinter import tkk
+
 
 #replace these values with the actual Serial port name and baud rate
 PORT = 'PLACEHOLDER'
 BAUD = 0000
 
 
+# with serial.Serial(port=PORT, baudrate=BAUD, timeout = 1) as ser :
+#     print(f"Connected to {ser.name}")
 
-with serial.Serial(port=PORT, baudrate=BAUD, timeout = 1) as ser :
-    print(f"Connected to {ser.name}")
-
-    while True:
-        raw_data = ser.readLine()
-        if raw_data:
-            clean_text = raw_data.decode('utf-8').strip()
-            print(f"Received: {clean_text}")
-
+#     while True:
+#         raw_data = ser.readLine()
+#         if raw_data:
+#             clean_text = raw_data.decode('utf-8').strip()
+#             print(f"Received: {clean_text}")
 
 
 
