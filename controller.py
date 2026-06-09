84c9e2bbd585cb9465537369bca82e47c52b30b0
cuttlefish27 <Sahil.Vijay624@gmail.com>
2026-06-08 22:37:20 -0700

    Added a controller file to be run on blender and made a simple object to store the data sent by Serial and send it to blender

---
+++ controller.py
@@ -0,0 +1,8 @@
+#CAN ONLY BE RUN IN A BLENDER INTERFACE
+import bpy
+
+
+dBone = bpy.data.objects["distal"]
+iBone = bpy.data.objects["intermediate"]
+pBone = bpy.data.objects["phalanx"]
+
--- viz.py
+++ viz.py
@@ -3,6 +3,24 @@ from tkinter import *
 from tkinter import tkk
 
 
+#class representing the current state of the thumb
+#curl represents the amount of curl in the finger(last two joints)
+#left and right represent the servo angles in the palm
+
+
+class Finger():
+    def __init__(self, curl, left, right):
+        self.curl = curl
+        self.left = left
+        self.right = right
+        
+
+def convertData(rawdata: bytes):
+    chars = rawdata.decode('utf-8')
+    curl, left, right = chars.split(',')
+    f1 = Finger(curl, left, right)
+    return f1
+
 #replace these values with the actual Serial port name and baud rate
 PORT = 'PLACEHOLDER'
 BAUD = 0000
@@ -14,8 +32,8 @@ BAUD = 0000
 #     while True:
 #         raw_data = ser.readLine()
 #         if raw_data:
-#             clean_text = raw_data.decode('utf-8').strip()
-#             print(f"Received: {clean_text}")
+#             finger1 = convertData(raw_data)
+
 
 
 
